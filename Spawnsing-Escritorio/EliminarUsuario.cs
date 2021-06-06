dc30bfe9dff76148bcb624a65a1be32573199423
JAE <j.alamaresparza@gmail.com>
2021-06-06 21:47:39 +0200

    Primera Pantalla y creación de los forms

--- Spawnsing-Escritorio/EliminarUsuario.cs
+++ Spawnsing-Escritorio/EliminarUsuario.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Spawnsing_Escritorio
 {
-    public partial class Form1 : Form
+    public partial class EliminarUsuario : Form
     {
-        public Form1()
+        public EliminarUsuario()
         {
             InitializeComponent();
         }
---
+++ Spawnsing-Escritorio/Inicio.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace Spawnsing_Escritorio
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.but = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(676, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenid@";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Spawnsing_Escritorio.Properties.Resources.Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(653, 209);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(317, 238);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(786, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 55);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "A";
+            // 
+            // but
+            // 
+            this.but.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.but.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.but.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.but.Location = new System.Drawing.Point(117, 553);
+            this.but.Name = "but";
+            this.but.Size = new System.Drawing.Size(227, 99);
+            this.but.TabIndex = 3;
+            this.but.Text = "Consulta Usuario";
+            this.but.UseVisualStyleBackColor = true;
+            this.but.Click += new System.EventHandler(this.but_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.button2.Location = new System.Drawing.Point(682, 553);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(228, 99);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Creación Usuario";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(1250, 553);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(227, 99);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Eliminación Usuario";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Spawnsing_Escritorio.Properties.Resources.backgroundbeta;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1671, 758);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.but);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "Inicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Spawnsign";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button but;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
